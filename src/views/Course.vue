e5a01af4c76b67541c0731e8d983ebb1dba758b8
Hamilton de Souza <hamilton-souza@outlook.com.br>
2022-01-19 09:48:53 -0300

    created component for course page, added new route, created course and courses page structure

---
+++ src/views/Course.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div v-if="loading">
+      <PageLoading />
+    </div>
+    <transition>
+      <div v-if="dataApi" class="content">
+        <div>
+          <h1>{{ dataApi.nome }}</h1>
+          <p>{{ dataApi.descricao }}</p>
+          <h2>Aulas</h2>
+          <ul class="aulas">
+            <li v-for="aula in dataApi.aulas" :key="aula.id">
+              {{ aula.nome }}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+import fetchData from "@/mixins/fetchData.js";
+
+export default {
+  name: "course",
+  props: ["curso"],
+  mixins: [fetchData],
+  created() {
+    this.fetchData(`/curso/${this.curso}`);
+  },
+};
+</script>
+
+<style>
+.aulas li {
+  display: block;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+  background: #fff;
+  padding: 20px;
+  margin-bottom: 10px;
+  border-radius: 4px;
+}
+</style>
--- src/views/Courses.vue
+++ src/views/Courses.vue
@@ -4,25 +4,37 @@
       <PageLoading />
     </div>
     <transition>
-      <div v-if="dataApi">
-        <h1>Cursos</h1>
-        {{dataApi}}
+      <div v-if="dataApi" class="content">
+        <div>
+          <h1>{{ dataApi.titulo }}</h1>
+          <p>{{ dataApi.descricao }}</p>
+        </div>
+        <ul>
+          <li v-for="curso in dataApi.cursos" :key="curso.id">
+            <h2>
+              <router-link :to="{ name: 'curso', params: { curso: curso.id } }"
+                >{{ curso.nome }} - {{ curso.totalAulas }} aulas |
+                {{ curso.horas }} horas</router-link
+              >
+            </h2>
+            <p>{{ curso.descricao }}</p>
+          </li>
+        </ul>
       </div>
     </transition>
   </div>
 </template>
 
 <script>
-import fetchData from '@/mixins/fetchData.js';
+import fetchData from "@/mixins/fetchData.js";
 export default {
-  name: 'courses',
+  name: "courses",
   mixins: [fetchData],
   created() {
     this.fetchData("/cursos");
-  }
-}
+  },
+};
 </script>
 
 <style>
-
 </style>
